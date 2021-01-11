cf5eb986e1a9963b88c507ddbd47af0b8bfbc267
judgeou <judgeou@vip.qq.com>
2021-01-11 11:18:49 +0800

    添加项目文件。

---
+++ NativeVIdeo/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int WINAPI WinMain (
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+) {
+	auto className = L"MyWindow";
+	WNDCLASSW wndClass = {};
+	wndClass.hInstance = hInstance;
+	wndClass.lpszClassName = className;
+	wndClass.lpfnWndProc = [](HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT {
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	};
+
+	RegisterClass(&wndClass);
+	auto window = CreateWindow(className, L"Hello World", WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(window, SW_SHOW);
+
+	MSG msg;
+	while (GetMessage(&msg, window, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
