8dcdbd0f320c127533f0110ee6ebd32c2429c138
judgeou <judgeou@vip.qq.com>
2021-01-13 16:06:41 +0800

    解码显示第一帧黑白画面

--- NativeVIdeo/main.cpp
+++ NativeVIdeo/main.cpp
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <vector>
+#include <string>
+
 #include <Windows.h>
+#include <ShlObj.h>
+#include <wrl.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#pragma comment(lib, "avcodec.lib")
+
+#include <libavformat/avformat.h>
+#pragma comment(lib, "avformat.lib")
+
+#include <libavutil/imgutils.h>
+#pragma comment(lib, "avutil.lib")
+
+#pragma comment(lib, "Bcrypt.lib")
+}
 
 using std::vector;
+using std::string;
+using std::wstring;
 
 struct Color_RGB
 {
@@ -10,6 +29,43 @@ struct Color_RGB
 	uint8_t g;
 	uint8_t b;
 };
+
+string w2s(const wstring& wstr) {
+	int len = WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), wstr.size(), NULL, 0, NULL, NULL);
+	string str(len, '\0');
+	WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), wstr.size(), &str[0], str.size(), NULL, NULL);
+	return str;
+}
+
+std::wstring AskVideoFilePath() {
+	using Microsoft::WRL::ComPtr;
+
+	ComPtr<IFileOpenDialog> fileDialog;
+
+	CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
+		IID_IFileOpenDialog, reinterpret_cast<void**>(fileDialog.GetAddressOf()));
+
+	fileDialog->SetTitle(L"选择视频文件");
+
+	COMDLG_FILTERSPEC rgSpec[] =
+	{
+		{ L"Video", L"*.mp4;*.mkv;*.flv;*.flac" },
+	};
+	fileDialog->SetFileTypes(1, rgSpec);
+	fileDialog->Show(NULL);
+
+	ComPtr<IShellItem> list;
+	fileDialog->GetResult(&list);
+
+	if (list.Get()) {
+		PWSTR pszFilePath;
+		list->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+		return pszFilePath;
+	}
+	else {
+		return L"";
+	}
+}
 
 void StretchBits (HWND hwnd, const vector<Color_RGB>& bits, int width, int height) {
 	auto hdc = GetDC(hwnd);
@@ -32,7 +88,65 @@ int WINAPI WinMain (
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd
 ) {
+	CoInitializeEx(NULL, COINIT::COINIT_MULTITHREADED);
 	SetProcessDPIAware();
+
+	auto filePath = w2s(AskVideoFilePath());
+
+	AVFormatContext* fmtCtx = nullptr;
+	avformat_open_input(&fmtCtx, filePath.c_str(), NULL, NULL);
+	avformat_find_stream_info(fmtCtx, NULL);
+
+	int videoStreamIndex;
+	AVCodecContext* vcodecCtx = nullptr;
+	for (int i = 0; i < fmtCtx->nb_streams; i++) {
+		AVCodec* codec = avcodec_find_decoder(fmtCtx->streams[i]->codecpar->codec_id);
+		if (codec->type == AVMEDIA_TYPE_VIDEO) {
+			videoStreamIndex = i;
+			vcodecCtx = avcodec_alloc_context3(codec);
+			avcodec_parameters_to_context(vcodecCtx, fmtCtx->streams[i]->codecpar);
+			avcodec_open2(vcodecCtx, codec, NULL);
+		}
+	}
+
+
+	AVFrame* firstframe;
+	while (1) {
+		AVPacket* packet = av_packet_alloc();
+		int ret = av_read_frame(fmtCtx, packet);
+		if (ret == 0) {
+			ret = avcodec_send_packet(vcodecCtx, packet);
+			if (ret == 0) {
+				AVFrame* frame = av_frame_alloc();
+				ret = avcodec_receive_frame(vcodecCtx, frame);
+				if (ret == 0) {
+					firstframe = frame;
+					break;
+				}
+				else if (ret == AVERROR(EAGAIN)) {
+
+				}
+
+				av_frame_unref(frame);
+			}
+		}
+
+		av_packet_unref(packet);
+	}
+
+	// 常见的都是YUV420p格式
+	AVPixelFormat format = (AVPixelFormat)firstframe->format;
+
+	int width = vcodecCtx->width;
+	int height = vcodecCtx->height;
+
+	vector<Color_RGB> pixels(width * height);
+	for (int i = 0; i < pixels.size(); i++) {
+		uint8_t r = firstframe->data[0][i];
+		uint8_t g = r;
+		uint8_t b = r;
+		pixels[i] = { r, g, b };
+	}
 
 	auto className = L"MyWindow";
 	WNDCLASSW wndClass = {};
@@ -44,14 +158,11 @@ int WINAPI WinMain (
 
 	RegisterClass(&wndClass);
 
-	int width = 800;
-	int height = 600;
 	auto window = CreateWindow(className, L"Hello World 标题", WS_OVERLAPPEDWINDOW, 0, 0, width, height, NULL, NULL, hInstance, NULL);
 
 	ShowWindow(window, SW_SHOW);
 
-	vector<Color_RGB> bits(width * height, { 255, 0, 0 });
-	StretchBits(window, bits, width, height);
+	StretchBits(window, pixels, width, height);
 
 	MSG msg;
 	while (GetMessage(&msg, window, 0, 0) > 0) {
@@ -59,5 +170,9 @@ int WINAPI WinMain (
 		DispatchMessage(&msg);
 	}
 
+	avcodec_free_context(&vcodecCtx);
+	avformat_close_input(&fmtCtx);
+
+	CoUninitialize();
 	return 0;
 }
